b1341f4ce61f091ae5fe26ff1edcf24333a8df6e
begenov <begenovorazali@icloud.com>
2023-05-10 16:37:07 +0600

    creating a writeJSON helper method

--- cmd/app/healtcheck.go
+++ cmd/app/healtcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,28 +13,11 @@ func (app *application) healthcheckHandler(ctx *gin.Context) {
 		"environment": app.config.env,
 		"version":     version,
 	}
-	var data1 map[string]string
-	js, err := json.Marshal(data)
+	err := app.writeJSON(ctx, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Printf(err.Error())
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"ERROR": "The server encountered a problem and could not process your request",
 		})
 		return
 	}
-
-	js = append(js, '\n')
-
-	err = json.Unmarshal(js, &data1)
-	if err != nil {
-		app.logger.Printf(err.Error())
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"ERROR": "The server encountered a problem and could not process your request",
-		})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"INFO": data1,
-	})
 }
--- cmd/app/helpers.go
+++ cmd/app/helpers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -13,4 +15,26 @@ func (app *application) readIDParam(params gin.Params) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(ctx *gin.Context, status int, data any, headers http.Header) error {
+	var data1 any
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		ctx.Writer.Header()[key] = value
+	}
+
+	err = json.Unmarshal(js, &data1)
+	if err != nil {
+		app.logger.Printf(err.Error())
+		return err
+	}
+	ctx.JSON(status, data1)
+	return nil
 }
