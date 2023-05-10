1ede04c72b33cf0143228fcafda28d4cef195c2b
begenov <begenovorazali@icloud.com>
2023-05-10 17:42:43 +0600

    json decoding

--- cmd/app/movies.go
+++ cmd/app/movies.go
@@ -22,13 +22,27 @@ func (app *application) healthcheckHandler(ctx *gin.Context) {
 }
 
 func (app *application) createMovieHandler(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "create a new movie")
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	if err := ctx.BindJSON(&input); err != nil {
+		app.errorResponse(ctx, http.StatusBadRequest, err.Error(), err)
+		return
+	}
+	ctx.String(http.StatusOK, "%+v\n", input)
+
 }
 
 func (app *application) showMovieHandler(ctx *gin.Context) {
 	id, err := app.readIDParam(ctx.Params)
 	if err != nil {
 		app.errorResponse(ctx, http.StatusNotFound, notFoundError, err)
+		return
 	}
 
 	movie := data.Movie{
