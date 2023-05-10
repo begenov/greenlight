edef4f0edfca11407bb38bbb7597e389a778f8d1
begenov <begenovorazali@icloud.com>
2023-05-10 17:00:10 +0600

    customizing the runtime field

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreateAt time.Time `json:"-"`
 	Title    string    `json:"title"`
 	Year     int32     `json:"year,omitempty"`
-	Runtime  int32     `json:"runtime,omitempty,string"`
+	Runtime  Runtime   `json:"runtime,omitempty,string"`
 	Genres   []string  `json:"genres,omitempty"`
 	Version  int32     `json:"version"`
 }
