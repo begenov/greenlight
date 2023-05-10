6f765e17b37d6ec061c7bc2ab3d69524ed4366fb
begenov <begenovorazali@icloud.com>
2023-05-10 16:28:42 +0600

    json encoding

--- cmd/app/healtcheck.go
+++ cmd/app/healtcheck.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,9 +9,33 @@ import (
 
 func (app *application) healthcheckHandler(ctx *gin.Context) {
 
-	js := fmt.Sprintf(`{"status": "available", "enironment": %q, "version": %q}`, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	var data1 map[string]string
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Printf(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"ERROR": "The server encountered a problem and could not process your request",
+		})
+		return
+	}
+
+	js = append(js, '\n')
+
+	err = json.Unmarshal(js, &data1)
+	if err != nil {
+		app.logger.Printf(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"ERROR": "The server encountered a problem and could not process your request",
+		})
+		return
+	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"INFO": js,
+		"INFO": data1,
 	})
 }
