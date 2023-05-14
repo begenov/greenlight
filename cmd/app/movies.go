8b7ea00524608f8b1cf74760ad00a2dedc227c33
begenov <begenovorazali@icloud.com>
2023-05-14 20:59:02 +0600

    the json unmarshal function

--- cmd/app/movies.go
+++ cmd/app/movies.go
@@ -24,10 +24,10 @@ func (app *application) healthcheckHandler(ctx *gin.Context) {
 func (app *application) createMovieHandler(ctx *gin.Context) {
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	if err := app.readJSON(ctx, &input); err != nil {
